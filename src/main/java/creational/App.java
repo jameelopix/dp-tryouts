4360dd157d29ff3fedc6b1de550f3e5a4bf94e8d
Mohamed Jameel <jameelopix@live.in>
2021-04-26 23:24:24 +0530

    initial code and git ignore setup

---
+++ src/main/java/creational/App.java
@@ -0,0 +1,14 @@
+package creational;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+
+		System.out.println("App.main()");
+
+		System.out.println("Hello World!");
+	}
+}
